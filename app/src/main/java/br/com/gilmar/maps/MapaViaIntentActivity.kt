19b2bb8cb5e6981eb9fd867f6d7b6a84836cb977
Logon Aluno <logonrmlocal@fiap.com.br>
2019-03-20 20:03:24 -0300

    novo app

---
+++ app/src/main/java/br/com/gilmar/maps/MapaViaIntentActivity.kt
@@ -0,0 +1,12 @@
+package br.com.gilmar.maps
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MapaViaIntentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mapa_via_intent)
+    }
+}
